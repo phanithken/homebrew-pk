f617164915485baca4005233de27c9636379871c
kenphanith <33407061+phanithken@users.noreply.github.com>
2023-06-08 16:04:17 +0900

    Update pk.rb

--- pk.rb
+++ pk.rb
@@ -2,7 +2,7 @@ class Pk < Formula
   desc "Nothing serious here!"
   homepage "https://github.com/phanithken/pk"
   url "https://github.com/phanithken/pk/releases/download/1.0.0/pk.tar.gz"
-  sha256 "5295d6a78c9401ef349e9030887083c612683818c3310a2ff50fc9452a0fc170"
+  sha256 "4e3314b39a9777a241f4abb5e1424047ec753f6a1d17d9e33dc48716fa0291d3"
   version "1.0.0"
   license "MIT"
 
