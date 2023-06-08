f93dbebd1f8552365327e09e8a41f41572ece067
kenphanith <33407061+phanithken@users.noreply.github.com>
2023-06-08 15:55:08 +0900

    Update and rename pkcli.rb to pk.rb

--- pk.rb
+++ pk.rb
@@ -1,7 +1,7 @@
 class Pkcli < Formula
   desc "Nothing serious here!"
-  homepage "https://github.com/phanithken/pkcli"
-  url "https://github.com/phanithken/pkcli/releases/download/1.0.3/pk.tar.gz"
+  homepage "https://github.com/phanithken/pk"
+  url "https://github.com/phanithken/pk/releases/download/1.0.3/pk.tar.gz"
   sha256 "eaea62b9462047b6b18ba51f62907d79445be4990cad4711b9af248549312e8e"
   version "1.0.3"
   license "MIT"
