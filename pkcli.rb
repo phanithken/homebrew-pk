c33163118ddb2d8755203b3b12f3b3732d3b8007
kenphanith <33407061+phanithken@users.noreply.github.com>
2023-06-08 15:46:51 +0900

    Update pkcli.rb

--- pkcli.rb
+++ pkcli.rb
@@ -1,8 +1,8 @@
 class Pkcli < Formula
   desc "Nothing serious here!"
   homepage "https://github.com/phanithken/pkcli"
-  url "https://github.com/phanithken/pkcli/releases/download/1.0.3/pkcli-mac.tar.gz"
-  sha256 "9f159c2d36732b35573d0d0f06b9657d5f2fe2a0b855afeed7d6ea680cd457ed"
+  url "https://github.com/phanithken/pkcli/releases/download/1.0.3/pkcli.tar.gz"
+  sha256 "9b53c02b2f115122de53d31c07382d209ab4417e5c3b3ba836449321896e74c0"
   version "1.0.3"
   license "MIT"
 
